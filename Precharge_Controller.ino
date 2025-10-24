a46292ae93f14f11b9e51615b21690f5caf35ac5
nb-defibrillator <bokurimono@gmail.com>
2025-10-24 02:10:26 -0400

    Initial commit

---
+++ Precharge_Controller.ino
@@ -0,0 +1,17 @@
+uint8_t AIR_Precharge = 2;
+uint8_t AIR_Main = 3;
+uint8_t AIR_3 = 4;
+
+
+
+bool CarRunning;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(AIR, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
