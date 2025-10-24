2238c20e8d14b32c7096612e6ba175cad96b95f6
nb-defibrillator <bokurimono@gmail.com>
2025-10-24 02:34:17 -0400

    Update Precharge_Controller.ino
    Setup pin names, exact spots subject to change based on ease of wiring.

--- Precharge_Controller.ino
+++ Precharge_Controller.ino
@@ -2,13 +2,20 @@ uint8_t AIR_Precharge = 2;
 uint8_t AIR_Main = 3;
 uint8_t AIR_3 = 4;
 
+uint8_t Optocoupler = 5;
+uint8_t BMS = 6;
 
 
 bool CarRunning;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(AIR, INPUT);
+  pinMode(AIR_Precharge, OUTPUT);    
+  pinMode(AIR_Main, OUTPUT);
+  pinMode(AIR_3, OUTPUT);
+  pinMode(Optocoupler, INPUT);
+  pinMode(BMS, INPUT);
+  CarRunning = false;
 }
 
 void loop() {
