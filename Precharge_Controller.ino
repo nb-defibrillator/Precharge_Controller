11ce83e724de76f39127f91ba1ae1d35db81ddd4
nb-defibrillator <bokurimono@gmail.com>
2025-10-25 19:58:51 -0400

    Precharge/Discharge logic sketched out
    Haven't gotten to test with a physical circuit, and the fault() function doesn't do anything right now, because I'm unsure what should be going on internally there.

--- Precharge_Controller.ino
+++ Precharge_Controller.ino
@@ -1,24 +1,88 @@
 uint8_t AIR_Precharge = 2;
 uint8_t AIR_Main = 3;
-uint8_t AIR_3 = 4;
+uint8_t AIR_Negative = 4;
+uint8_t AIR_Discharge = 5;
+uint8_t LED_Discharge = 9;
 
-uint8_t Optocoupler = 5;
-uint8_t BMS = 6;
+uint8_t Optocoupler = 6;
+uint8_t BMS = 7;
+uint8_t Switch2 = 8;
+
+bool carRunning = false;
+bool prechargeFailed = false;
+bool dischargeFinished = false;
+
+unsigned long prechargeStart=-1;
+const unsigned long prechargeTimeout = 5e6; // 5s
+const unsigned long prechargeTime = 5e5; // 0.5s
+unsigned long lastValidStart = -1; 
+
+unsigned long dischargeStart = -1;
+const unsigned long dischargeInterval = 7.79e7; // 77.9s
 
 
-bool CarRunning;
+
+void precharge() {
+  if (~prechargeFailed){
+    digitalWrite(AIR_Precharge, HIGH);
+    digitalWrite(AIR_Negative, HIGH);
+  }
+  if (digitalRead(Optocoupler)) {
+    unsigned long now = micros();
+    
+    //This part waits for a steady signal from the optocoupler, 
+    //current duration .5s but that's just a guess.
+    if (lastValidStart == -1) lastValidStart = now;
+    else if (now - lastValidStart > prechargeTime) {
+      carRunning = true;
+      digitalWrite(AIR_Precharge, LOW);
+    }
+    //timeout check
+    else if (now - prechargeStart > prechargeTimeout) { 
+      prechargeFailed = true;
+      fault();
+    }
+  } else lastValidStart = -1;
+}
+
+void discharge() {
+  digitalWrite(AIR_Discharge, HIGH);
+  unsigned long now = micros();
+  if (now - dischargeStart > dischargeInterval) {
+    digitalWrite(LED_Discharge, HIGH);
+    dischargeFinished = true;
+  }
+}
+
+void fault() {
+  //still not sure what this should do
+}
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(AIR_Precharge, OUTPUT);    
   pinMode(AIR_Main, OUTPUT);
-  pinMode(AIR_3, OUTPUT);
+  pinMode(AIR_Negative, OUTPUT);
+  pinMode(AIR_Discharge, OUTPUT);
+  pinMode(LED_Discharge, OUTPUT);
   pinMode(Optocoupler, INPUT);
+  
   pinMode(BMS, INPUT);
-  CarRunning = false;
+  pinMode(Switch2, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (carRunning == false) {
+      if (digitalRead(Switch2) == HIGH && digitalRead(BMS) == HIGH) {
+        if (prechargeStart == -1) prechargeStart = micros();
+        precharge();
+      }
+  }
+  else {
+    if (~Switch2) {
+      if (dischargeStart = -1) dischargeStart = micros();
+      if (dischargeFinished == false) discharge();
+    }
+  }
 }
